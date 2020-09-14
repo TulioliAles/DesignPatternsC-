b4a70f1686117860dc2e98ed1f79af29c8b6eb2b
TulioliAles <desmecad@hotmail.com>
2020-09-14 20:59:20 -0300

    Commit 4 - Exercicio Adapter

---
+++ 2-Structural/2.1-Adapter/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns._2_Structural._2._1_Adapter
+{
+    public interface ILogger
+    {
+        void Log(string message);
+        void LogError(Exception exception);
+    }
+}
---
+++ 2-Structural/2.1-Adapter/LogNetMasterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns._2_Structural._2._1_Adapter
+{
+    public class LogNetMasterService : ILogNetMaster
+    {
+        public void LogInfo(string message)
+        {
+            Console.WriteLine("Log Customizado - " + message);
+        }
+
+        public void LogException(Exception exception)
+        {
+            Console.WriteLine("Log Customizado - " + exception.Message);
+        }
+    }
+}
